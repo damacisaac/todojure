cb2faa426115c949d7f954af230923e862eca357
Angus <donald.macisaac@mail.mcgill.ca>
2015-02-08 11:02:34 -0500

    Use new connection for schema, print transaction responses

--- src/clj/om_async/init_db.clj
+++ src/clj/om_async/init_db.clj
@@ -8,12 +8,13 @@
 
 (def conn (d/connect uri))
 
-(def db (d/db conn))
+(defn get-conn []
+  (d/connect uri))
 
 (defn get-schema []
-  (sort (d/q '[:find ?ident
+  (sort (vec (d/q '[:find ?ident
                :where [_ :db/ident ?ident]]
-             db)))
+             (d/db (get-conn))))))
 
 (defn read-all [f]
   (Util/readAll (io/reader f)))
@@ -26,14 +27,11 @@
 (defn create-db []
   (d/create-database uri))
 
-(defn get-conn []
-  (d/connect uri))
-
 (defn load-schema []
-  (transact-all (get-conn) (io/resource "data/schema.edn")))
+  (println (transact-all (get-conn) (io/resource "data/schema.edn"))))
 
 (defn load-data []
-  (transact-all (get-conn) (io/resource "data/initial.edn")))
+  (println (transact-all (get-conn) (io/resource "data/initial.edn"))))
 
 (defn init-db []
   (create-db)
